20128923c70b6ffda638311b0d97e0f769b9e0a0
HibbaHindawi <hibba04@hotmail.se>
2024-05-26 02:38:59 +0200

    yuh

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let resultElem;
+let lesbianAudio = new Audio("Audio/lesbian.mp3");
+let gayAudio = new Audio("Audio/gay.mp3");
+let straightAudio = new Audio("Audio/straight.mp3");
 
 function init() {
 resultElem = document.querySelector("#result");
@@ -25,7 +28,30 @@ function calculate() {
     else if(percentage >= 50){
         resultElem.innerHTML += "<img src='Images/touch.png' alt='Touching Cat'><p> you will be touched tonight ;) </p>";
     }
+    let parent = resultElem.parentNode;
+    if(parent.id == "lesbian"){
+        lesbianAudio.loop = true;
+        lesbianAudio.volume = 0.2;
+        lesbianAudio.play();
+    }
+    else if(parent.id == "gay"){
+        gayAudio.loop = true;
+        gayAudio.volume = 0.2;
+        gayAudio.play();
+    }
 }
 function straightCalculate(){
+    initMap();
+    straightAudio.loop = true;
+    straightAudio.volume = 0.5;
+    straightAudio.play();
     resultElem.innerHTML = "<p> 0%</p>";
+}
+function initMap(){
+    let map = L.map('map').locate({setView: true, watch: true, maxZoom: 17});
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 }
